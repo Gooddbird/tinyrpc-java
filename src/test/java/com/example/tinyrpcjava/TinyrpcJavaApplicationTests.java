0cdc8a9d92afeae8203e9af9e62c42b08575c042
Gooddbird <1753009868@qq.com>
2023-03-03 22:05:45 +0800

    first init

---
+++ src/test/java/com/example/tinyrpcjava/TinyrpcJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinyrpcjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyrpcJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
